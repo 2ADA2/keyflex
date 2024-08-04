0861a2278918f214a0dc6ff9771487b320f59171
ada2 <87185550+2ADA2@users.noreply.github.com>
2024-08-04 23:26:10 +0300

    keyflex v0.5

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -18,7 +18,7 @@ import {
   ApexXAxis,
 } from "ng-apexcharts";
 
-import {countChart, mainChart} from "../../utils/chartOptions";
+import {barChartOptions, countChart, mainChart} from "../../utils/chartOptions";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faGear} from "@fortawesome/free-solid-svg-icons";
 
@@ -68,6 +68,8 @@ export class UserComponent {
   public accuracyChartSeries = [100,64,59,90,36,43];
   public attendanceChartSeries = [1001, 304, 96, 201, 17, 30];
 
+  public lastResults : any
+
   constructor() {
     this.countChartOptions = {
       ...countChart,
@@ -77,7 +79,19 @@ export class UserComponent {
     this.mainChart = {
       ...mainChart
     }
+
+    this.lastResults = {
+      ...barChartOptions,
+      xaxis: {
+        categories: ['200 слов', 'Пользовательский', '200 слов', '200 слов', '200 слов', '1000 слов', '200 слов', '200 слов', '200 слов', '200 слов'],
+      },
+      series: [{
+        data: [70, 35, 65, 67, 75, 80, 69, 61, 71, 75]
+      }],
+
+    }
   }
 
   protected readonly faGear = faGear;
+  protected readonly barChartOptions = barChartOptions;
 }
--- src/app/utils/chartOptions.ts
+++ src/app/utils/chartOptions.ts
@@ -5,36 +5,45 @@ export const mainChart: ChartLineOptions = {
     {
       name: "200 слов",
       type: 'line',
-      data: [{ x: 1, y: 0 }, { x: 2, y: 62 }, { x: 3, y: 61 }, { x: 4, y: 63 }, { x: 5, y: 62 }, { x: 6, y: 70 }, { x: 7, y: 58 }, { x: 8, y: 65 }, { x: 9, y: 60 }, { x: 10, y: 72 }],
-      color:"#4fd9c6"
+      data: [{x: 1, y: 0}, {x: 2, y: 62}, {x: 3, y: 61}, {x: 4, y: 63}, {x: 5, y: 62}, {x: 6, y: 70}, {
+        x: 7,
+        y: 58
+      }, {x: 8, y: 65}, {x: 9, y: 60}, {x: 10, y: 72}],
+      color: "#4fd9c6"
     },
     {
       name: "1000 слов",
       type: 'line',
-      data: [{ x: 1, y: 0 }, { x: 2, y: 46 }, { x: 3, y: 53 }, { x: 4, y: 49 }, { x: 5, y: 59 }, { x: 6, y: 41 }, { x: 7, y: 47 }, { x: 8, y: 45 }, { x: 9, y: 39 }, { x: 10, y: 41 }],
-      color:"#bce544"
+      data: [{x: 1, y: 0}, {x: 2, y: 46}, {x: 3, y: 53}, {x: 4, y: 49}, {x: 5, y: 59}, {x: 6, y: 41}, {
+        x: 7,
+        y: 47
+      }, {x: 8, y: 45}, {x: 9, y: 39}, {x: 10, y: 41}],
+      color: "#bce544"
     },
     {
       name: "Текст",
       type: 'line',
-      data: [{ x: 1, y: 0 }, { x: 2, y: 41 }, { x: 3, y: 36 }, { x: 4, y: 29 }, { x: 5, y: 35 }, { x: 6, y: 37 }, { x: 7, y: 27 }, { x: 8, y: 36 }, { x: 9, y: 40 }, { x: 10, y: 34 }],
-      color:"#f146d8"
+      data: [{x: 1, y: 0}, {x: 2, y: 41}, {x: 3, y: 36}, {x: 4, y: 29}, {x: 5, y: 35}, {x: 6, y: 37}, {
+        x: 7,
+        y: 27
+      }, {x: 8, y: 36}, {x: 9, y: 40}, {x: 10, y: 34}],
+      color: "#f146d8"
     }
   ],
   stroke: {
-    width: [3,3,3], // Устанавливаем ширину линий
+    width: [3, 3, 3], // Устанавливаем ширину линий
     curve: 'smooth' // Сглаживание линий
   },
   chart: {
     type: 'line', // Устанавливаем тип графика как 'line'
     height: 500,
-    background:"#1c1c1c",
-    foreColor:"#fff",
-    animations: {enabled:false}
+    background: "#1c1c1c",
+    foreColor: "#fff",
+    animations: {enabled: false}
   },
   xaxis: {
-    min:0,
-    max:100,
+    min: 0,
+    max: 100,
     labels: {
       show: false
     }
@@ -43,8 +52,8 @@ export const mainChart: ChartLineOptions = {
     min: 0,
     max: 100
   },
-  grid:{
-    show:false
+  grid: {
+    show: false
   },
   tooltip: {
     theme: 'dark', // Темная тема для всплывающей подсказки
@@ -52,8 +61,8 @@ export const mainChart: ChartLineOptions = {
       fontSize: '12px',
       fontFamily: 'Arial, sans-serif'
     },
-    x:{
-      formatter: function (val:number){
+    x: {
+      formatter: function (val: number) {
         return "Результат №" + val
       }
     }
@@ -61,11 +70,9 @@ export const mainChart: ChartLineOptions = {
 }
 
 
-
 export const countChart: any = {
   chart: {
     type: "polarArea",
-    animations: { enabled: false },
     height: 300
   },
   colors: ["#00c776", "#b2c91c", "#ff7200", "#1924B1", "#D60062", "#360570"],
@@ -110,4 +117,55 @@ export const countChart: any = {
   labels: ["200 слов", "1000 слов", "Текст", "Английский", "Символьный", "Пользовательский"]
 };
 
+export const barChartOptions: any = {
+  chart: {
+    type: 'bar',
+    height: 350,
+    background: "#1c1c1c",
+    foreColor: "#fff",
+    toolbar: {
+      show: false
+    }
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '55%',
+      endingShape: 'rounded',
+      distributed: true // Распределение цветов по каждому столбцу
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent']
+  },
+  fill: {
+    opacity: 1
+  },
+  tooltip: {
+    theme: 'dark',
+    shared: false,
+    style: {
+      fontSize: '12px',
+      fontFamily: 'Arial, sans-serif',
+      foreColor: "#000",
+      background: "#000"
+    },
+    y: {
+      title: {
+        formatter: function () {
+          return 'Слов в минуту:';
+        }
+      },
+    }
+  },
+  colors: ['#33FF57', '#AF33FF', '#33FF57', '#33FF57', '#33FF57', '#FFA833', '#33FF57', '#33FF57', '#33FF57', '#33FF57'],
+  legend: {
+    show: false
+  }
+};
 
